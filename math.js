6e07c914f24b846138f7f432630299150e596fb0
NovMarina <marina.novikova1998@gmail.com>
2025-05-25 22:13:33 +0300

    second commit

--- math.js
+++ math.js
@@ -16,6 +16,42 @@ function translate(number) {
     }
 }
 
+function binary(number) {
+    if (number === 0)
+        return "0";
+    let sign = "";
+    if (number < 0) {
+        sign = "-";
+        number = -number;
+    }
+    let result = "";
+    while (number !== 0) {
+        result += (number % 2);
+        number = parseInt(number / 2);
+    }
+    return sign + result;
+}
+
+console.log(binary(numberStr));
+
+function hex(number) {
+    if (number === 0) return "0";
+    let sign = "";
+    if (number < 0) {
+        sign = "-";
+        number = -number;
+    }
+    const digits = "0123456789abcdef";
+    let result = "";
+    while (number !== 0) {
+        let i = number % 16;
+        result += digits[i];
+        number = parseInt(number / 16);
+    }
+    return sign + result;
+}
+console.log(hex(numberStr));
+
 translate(numberStr);
 
 function reverse(number) {
@@ -45,6 +81,8 @@ function sumAllNumbers(number) {
 }
 
 sumAllNumbers(numberStr);
+
+
 
 
 
